7d6427c8487886f7b7acd12cb59315efe5a54c81
Michael Reed <mreed5812@gmail.com>
2020-12-24 22:26:04 -0600

    added shift to string parameter

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,3 +1,29 @@
-#remember to convert a string into a number
-#wrap from z to a
-#don't forget to keep the same case
+#takes 2 inputs -> string and numerical key
+
+#Get string from user
+print "Enter phrase: "
+phrase = gets.chomp
+
+#Get key from user
+print "Enter key: "
+key = gets.chomp
+
+
+
+def caesar_cipher(string, key)
+    #1. create variables to store string,key values
+    s = string.upcase
+    k = key.to_i
+    
+    #2. break string into array of char
+    phrase_array = s.chars
+
+    #3. convert each char to a number and store in new array
+    phrase_array.map! { |c| c.ord + k}
+
+    puts phrase_array
+    #4. shift each number by key
+    #5. convert each number back to char
+end
+
+caesar_cipher(phrase, key)
