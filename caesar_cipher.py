0ed34d4d8aeeb2fcace533590f41a49c2d3a80f9
Michael Reed <michaelreed@michaels-mbp.lan>
2023-01-26 07:30:09 -0600

    adjusted logic to maintain spacing in phrases

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -12,22 +12,23 @@ def caesar_cipher(phrase, shift):
     
     # split phrase into array of characters
     phrase_arr = [ord(x) for x in phrase]
-    print(phrase_arr)
     cipher_arr = []
-    
     
     for c in phrase_arr:
         base = 0
-        if c < 91: 
+
+        if c in range (65,91): 
             base = 65
-        else:
+        elif c in range(97,123):
             base = 97
-        if base in range(65, 90) or base in range(97, 122): 
+
+        if c in range(65, 91) or base in range(97, 123): 
             cipher_arr.append((((c - base) + shift) % 26) + base)
+        else: 
+            cipher_arr.append(c)
+
+    result = ''.join([chr(x) for x in cipher_arr])    
+    print(result)
 
 
-    print(cipher_arr)
-
-
-
-caesar_cipher("Zebra", 5)
+caesar_cipher("This is Z test", 5)
